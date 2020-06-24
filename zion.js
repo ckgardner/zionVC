eafa7a5150f39e2f504dcdef5fff762548ce134b
treydin6 <treydinw@gmail.com>
2020-06-23 20:18:40 -0600

    working on date drop downs

--- zion.js
+++ zion.js
@@ -11,11 +11,17 @@ var app = new Vue({
         visitorOutPeople: 88,
         dailyTotalPeople: 642,
         occupancyPeople: 42,
-
+        reportTimeIntervalList: ['15 min', '1 hour', '24 hour total'],
+        reportTimeInterval: '15 min',
         currentIP: "190.168.1.90",
+        stateArrowImage: 'icons/downArrow.png',
+        DatePickerPopUp: false,
+        reportDate: '',
+        fullDate: ''
     },
     created: function(){
         this.loadMainStats();
+        this.getCurrentDate();
     },
     methods: {
         loadMainStats: function() {
@@ -33,5 +39,69 @@ var app = new Vue({
             this.currentIP = currentIp;
             this.page = 'dashboard';
         },
+        switchArrow: function() {
+            if(this.stateArrowImage == 'icons/downArrow.png'){
+                this.stateArrowImage = 'icons/upArrow.png';
+            } else{
+                this.stateArrowImage = 'icons/downArrow.png';
+            }
+        },
+        resetArrow: function() {
+            this.stateArrowImage = 'icons/downArrow.png';
+        },
+        closeDatePicker: function() {
+            this.DatePickerPopUp = false;
+        },
+        openDatePicker: function() {
+            this.DatePickerPopUp = true;
+        },
+        getCurrentDate: function() {
+            // jul 16 2020
+            var today = new Date();
+            var day = today.getDate();
+            var monthNumber = today.getMonth()
+            var year = today.getFullYear();
+            var month = this.getMonthToString(monthNumber);
+            
+            this.fullDate = `${month} ${day} ${year}`;
+            // console.log('report date: ', this.reportDate);
+            console.log(`full ${year} ${day} ${month}`);
+
+        },
+        DateSelected: function() {
+            let year1 = this.reportDate.substr(0,4);
+            let month1 = this.reportDate.substr(5,2);
+            console.log(`month "${month1}" `, month1.length)
+            var monthString = this.getMonthToString(Number(month1));
+            console.log('month string val ', monthString)
+            let day1 = this.reportDate.substr(8,2);
+            console.log('month: ', month1);
+            this.fullDate =  `${monthString} ${day1} ${year1}`;
+            console.log(this.fullDate);
+            console.log('monthstring', monthString);
+        },
+        getMonthToString: function(monthNumber) {
+            console.log('in month function: ', monthNumber);
+            var month = '';
+            switch(monthNumber) {
+                case 0:
+                    month = 'Jan'; 
+                    break;
+                case 1: month = 'Feb'; break;
+                case 2: month = 'Mar'; break;
+                case 3: month = 'April'; break;
+                case 4: month = 'May'; break;
+                case 5: month = 'Jun'; break;
+                case 6: month = 'Jul'; break;
+                case 7: month = 'Aug'; break;
+                case 8: month = 'Sept'; break;
+                case 9: month = 'Oct'; break;
+                case 10: month = 'Nov'; break;
+                case 11: month = 'Dec'; break;
+            }
+            console.log('month: ', month);
+            return month;
+        }
     }
+
 });
